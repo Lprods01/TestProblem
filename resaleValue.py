8d9a93fa6cb0a612d41e9fbebeefedd3dae7ed0b
Jenerth Rodriguez <Connerjamestill@gmail.com>
2023-10-20 13:34:35 -0400

    fixing the previous error and comming with the right answer

--- resaleValue.py
+++ resaleValue.py
@@ -3,9 +3,10 @@ import os
 
 def clean_and_summarize_csv(input_file):
     df = pd.read_csv(input_file)
-    df.dropna(inplace=True)
+    df.dropna(subset=["Resale Value"], inplace=True)
+    df["Resale Value"] = df["Resale Value"].str.replace('[$,]', '', regex=True).astype(float)
     total_resale_value = df["Resale Value"].sum()
-    resale_value_by_item = df.groupby("Item")["Resale Value"].sum()
+    resale_value_by_item = df.groupby("Item")["Resale Value"].sum().reset_index()
     return total_resale_value, resale_value_by_item
 
 def write_result_to_file(result, output_file):
@@ -16,9 +17,16 @@ def main():
     input_file = "TestData.csv"
     output_directory = "output"
     os.makedirs(output_directory, exist_ok=True)
+
     total_resale_value, resale_value_by_item = clean_and_summarize_csv(input_file)
-    write_result_to_file(f"Total Resale Value: {total_resale_value}", os.path.join(output_directory, "total_resale_value.txt"))
-    write_result_to_file("Resale Value by Item:\n" + resale_value_by_item.to_string(), os.path.join(output_directory, "resale_value_by_item.txt"))
+
+    total_resale_value_output_file = os.path.join(output_directory, "ResaleValueTotal.csv")
+    resale_value_by_item_output_file = os.path.join(output_directory, "ResaleValueByItem.csv")
+
+    total_resale_value_df = pd.DataFrame({"Resale Value Total": [total_resale_value]})
+    total_resale_value_df.to_csv(total_resale_value_output_file, index=False)
+
+    resale_value_by_item.to_csv(resale_value_by_item_output_file, index=False)
 
 if __name__ == "__main__":
     main()
